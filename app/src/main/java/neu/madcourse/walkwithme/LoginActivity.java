6ed5cc759418c8d2299acff615e40ef26f5fdc73
Jelly <zoe.xiaoqing@gmail.com>
2020-11-23 14:40:19 -0500

    finish login and register

--- app/src/main/java/neu/madcourse/walkwithme/LoginActivity.java
+++ app/src/main/java/neu/madcourse/walkwithme/LoginActivity.java
@@ -3,19 +3,76 @@ package neu.madcourse.walkwithme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
-    
+    private EditText userName;
+    private EditText password;
+    private Button loginButton;
+    private Button registerButton;
+    private DatabaseReference mDatabase;
+
+    private static final String TAG = "Login page";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        myRef.setValue("Hello, World!");
+        mDatabase = FirebaseDatabase. getInstance ().getReference();
+        userName = (EditText) findViewById(R.id.editTextLogin);
+        password = (EditText) findViewById(R.id.editTextPassword );
+        loginButton = (Button) findViewById(R.id.submit );
+        registerButton = (Button) findViewById(R.id.register );
     }
+
+    public void onRegisterClick(View view) {
+        final String username = userName.getText().toString();
+        final String password = Utils.md5Encryption(this.password.getText().toString());
+        final User user = new User(username, password, System.currentTimeMillis());
+        mDatabase.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChild(username)) {
+                    Log.i( TAG, "" + getApplicationContext());
+                    Toast.makeText(getApplicationContext(),"username is already registered", Toast.LENGTH_SHORT).show();
+                } else if (username.length() != 0 && password.length() != 0){
+                    mDatabase.child("users").child(user.getUsername()).setValue(user);
+                    Toast.makeText(getApplicationContext(),username + " successfully registered", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
+    public void onLoginClick(View view) {
+        final String username = userName.getText().toString();
+        final String password = Utils.md5Encryption(this.password.getText().toString());
+        mDatabase.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChild(username) &&
+                        (password.equals(dataSnapshot.child(username).child("password").getValue()))) {
+                    Log.i( TAG, "You successfully login");
+                } else {
+                    Toast.makeText(getApplicationContext(),"Please login again", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/neu/madcourse/walkwithme/Utils.java
@@ -0,0 +1,24 @@
+package neu.madcourse.walkwithme;
+
+import org.apache.commons.codec.binary.Hex;
+
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+public class Utils {
+    public static String md5Encryption(final String input) {
+        String result = "";
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.reset();
+            messageDigest.update(input.getBytes(Charset.forName("UTF8")));
+            byte[] resultByte = messageDigest.digest();
+            StringBuffer hexString = new StringBuffer();
+            for (int i=0; i<resultByte.length; i++)
+                hexString.append(Integer.toHexString(0xFF & resultByte[i]));
+            return hexString.toString();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+}
