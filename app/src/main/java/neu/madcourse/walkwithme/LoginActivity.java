3ee1d3b75cb12adb198c32db69704e1744d7f247
Jelly <zoe.xiaoqing@gmail.com>
2020-11-23 12:31:51 -0500

    Create login activity

---
+++ app/src/main/java/neu/madcourse/walkwithme/LoginActivity.java
@@ -0,0 +1,20 @@
+package neu.madcourse.walkwithme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
+    }
+}
--- app/src/main/java/neu/madcourse/walkwithme/MainActivity.java
+++ app/src/main/java/neu/madcourse/walkwithme/MainActivity.java
@@ -2,13 +2,26 @@ package neu.madcourse.walkwithme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = (Button) findViewById(R.id.btnLogin);
+
+    }
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnLogin:
+                Intent buttonGrid = new Intent(this, LoginActivity.class);
+                startActivity(buttonGrid);
+                break;
+        }
     }
 }
