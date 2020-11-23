1dc5980543588cfd6cd3eef4e6f79ea5d12f4ae2
Jelly <zoe.xiaoqing@gmail.com>
2020-11-23 13:16:32 -0500

    login ui

--- app/src/main/java/neu/madcourse/walkwithme/LoginActivity.java
+++ app/src/main/java/neu/madcourse/walkwithme/LoginActivity.java
@@ -8,6 +8,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
