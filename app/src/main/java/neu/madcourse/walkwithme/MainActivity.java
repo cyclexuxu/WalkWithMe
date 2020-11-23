d52f71d9a578dbe8ee3c91304fc37464d714473b
Dan Yin <danyin@Dans-MacBook-Pro.local>
2020-11-23 12:05:10 -0800

    pedometer activity added

--- app/src/main/java/neu/madcourse/walkwithme/MainActivity.java
+++ app/src/main/java/neu/madcourse/walkwithme/MainActivity.java
@@ -2,7 +2,9 @@ package neu.madcourse.walkwithme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void startProgressActivity(View view){
+        startActivity(new Intent(MainActivity.this, ProgressActivity.class));
     }
 }
