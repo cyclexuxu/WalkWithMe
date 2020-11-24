3de4a2584d9ac6a8003de4a69b27e1dcea025ff3
Dan Yin <danyin@Dans-MacBook-Pro.local>
2020-11-23 16:05:27 -0800

    first version pedometer, not accurate

--- app/src/main/java/neu/madcourse/walkwithme/MainActivity.java
+++ app/src/main/java/neu/madcourse/walkwithme/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import neu.madcourse.walkwithme.stepcounter.ProgressActivity;
+
 public class MainActivity extends AppCompatActivity {
     private Button login;
     @Override
