3a1d7961d9f7e3a5dc9bc6a3e36e9cd6c8087979
Xu Yuan <sysuyxx@gmail.com>
2020-12-02 15:57:20 -0800

    change ranking to fragment

--- app/src/main/java/neu/madcourse/walkwithme/ranking/DRankingData.java
+++ app/src/main/java/neu/madcourse/walkwithme/ranking/DRankingData.java
@@ -1,23 +1,27 @@
 package neu.madcourse.walkwithme.ranking;
 
+import android.annotation.SuppressLint;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
-import neu.madcourse.walkwithme.userlog.User;
+import neu.madcourse.walkwithme.userlog.LoginActivity;
 
 public class DRankingData {
 
@@ -25,15 +29,56 @@ public class DRankingData {
     private List<String> usernames;
     private List<ItemRank> itemRanks;
     private String LOG = "DRankingData";
+    private String today;
+    protected int currentUserStep;
+
     // private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
 
+    @SuppressLint("SimpleDateFormat")
     public DRankingData() {
+//        Date date = new Date();
+//        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+//        today = dateFormat.format(date);
+        today = new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
+        countCurrentUserSteps();
 
         databaseReference = FirebaseDatabase.getInstance().getReference("users");
         usernames = new ArrayList<>();
         getUsername();
+
+
+        Log.d(LOG, today);
+
+        // today = new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
         // Log.d(LOG, user.getDisplayName());
+
+    }
+
+    private void countCurrentUserSteps() {
+        String currentUsername = LoginActivity.currentUser;
+
+        // DatabaseReference databaseCurrentUser = FirebaseDatabase.getInstance().getReference("Users").child(currentUsername).child("Step Count").child(today);
+        DatabaseReference databaseCurrentUser = FirebaseDatabase.getInstance().getReference("users").child(currentUsername).child("Step Count");
+
+
+        databaseCurrentUser.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
+
+                    String steps = dataSnapshot.child("steps").getValue().toString();
+                    Log.d(LOG, steps);
+                    currentUserStep = Integer.parseInt(steps);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
     public void getUsername() {
@@ -59,8 +104,13 @@ public class DRankingData {
         itemRanks = new ArrayList<>();
         Random random = new Random();
         for (String username : usernames) {
-            int steps = random.nextInt(20000);
-            int likes = random.nextInt(50);
+            int steps = random.nextInt(15000);
+            int likes = random.nextInt(20);
+            if (username.equals(LoginActivity.currentUser)) {
+                steps = currentUserStep;
+                Log.d(LOG, String.valueOf(steps));
+            }
+
             // Log.d(LOG, String.valueOf(steps));
             ItemRank itemRank = new ItemRank(username, steps, likes);
             itemRanks.add(itemRank);
